804eb6746a7e1bd2d1b308d38205c286b4971cfc
supriya <supriya.panda243@gmail.com>
2026-06-13 00:18:18 +0530

    Update database configuration for production(allowed_hosts)

--- djangofullpro/djangofullpro/settings.py
+++ djangofullpro/djangofullpro/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS=[
-"13.234.30.244"
+    "13.234.30.244",
+    "localhost",
+    "127.0.0.1"
 ]
 
 # Application definition
