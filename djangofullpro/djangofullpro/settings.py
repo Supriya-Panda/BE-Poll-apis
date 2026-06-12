ddcbca9de5076be02116caa27d80e206a3f7eab3
supriya <supriya.panda243@gmail.com>
2026-06-12 21:40:48 +0530

    Update database configuration for production1

--- djangofullpro/djangofullpro/settings.py
+++ djangofullpro/djangofullpro/settings.py
@@ -15,14 +15,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from decouple import config
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = os.getenv('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS=[
 "pollapi.com",
